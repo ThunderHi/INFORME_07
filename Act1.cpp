c708e793cf989812ee3750f4950c82f450e192b8
ThunderHi <161666073+ThunderHi@users.noreply.github.com>
2024-05-15 21:30:20 -0500

    En progreso de Actividades

--- Act1.cpp
+++ Act1.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 using namespace std;
+//Clase cliente de entidad bancaria
 class Cliente{
+    //Atributos protegidos
     protected:
-        string nombre;
+        string nombre;  
         string direccion;
         string numeroTarjeta;
         string contraseña;
     public:
+        //Constructor
         Cliente(string _nombre, string _direccion, string _numeroTarjeta, string _contraseña){
-            
+            this->nombre = _nombre;
+            this->nombre = _direccion;
+            this->nombre = _numeroTarjeta;
+            this->nombre = _contraseña;
         }
 };
-class ClienteSeguro:public Cliente{
+
+//Clase derivada de Cliente
+class ClienteSeguro:public Cliente{     //La clase hija puede acceder a lo publico de padre
+    
 
 };
