59e60695f3261e54d5cc2067145b0220626c4cf6
AngelSalasZapata <164601086+AngelSalasZapata@users.noreply.github.com>
2024-05-16 17:45:43 -0500

    -

--- Eje2.cpp
+++ Eje2.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-class Persona { //declaramos la clase Persona
+class Persona{ //declaramos la clase Persona
     protected: //atributo protegido "email"
     string email;
     public: //atributos publicos nombre, edad y salario
@@ -13,7 +13,7 @@ class Persona { //declaramos la clase Persona
     Persona(string nom, int ed, float sal, string correo) //constructor extendido de la clase Persona
         :nombre(nom), edad(ed), salario(sal), email(correo) {} //inicializacion de los atributos
 
-    void mostrarInfo() { //funcion miembro publica para mostrar la informacion de cada Persona
+    void mostrarInfo(){ //funcion miembro publica para mostrar la informacion de cada Persona
         cout<< "Nombre: "<<nombre << endl;
         cout<< "Edad: "<<edad << endl;
         cout<<"Salario: "<<salario << endl; 
@@ -21,7 +21,7 @@ class Persona { //declaramos la clase Persona
     }
 };
 
-class Empleado : public Persona { //clase Empleado heredada de la clase Persona
+class Empleado : public Persona{ //clase Empleado heredada de la clase Persona
 public:
     Empleado(string nom, int ed, float sal, string correo) //constructor de la clase Empleado
         :Persona(nom, ed, sal, correo) {}
@@ -31,17 +31,17 @@ public:
     }
 };
 
-class Cliente : public Persona { //clase Cliente heredada de la clase Persona
+class Cliente : public Persona{ //clase Cliente heredada de la clase Persona
 public:
     Cliente(string nom, int ed, float sal, string correo) //constructor de la clase Cliente
         :Persona(nom, ed, sal, correo) {}
         
-    string getCorreo() { //metodo getter para obtener acceso al atributo protegido email
+    string getCorreo(){ //metodo getter para obtener acceso al atributo protegido email
         return email;
     }    
 };
 
-int main() { //creamos objetos emp1 y cl1 de las clases Empleado y Cliente respectivamente
+int main(){ //creamos objetos emp1 y cl1 de las clases Empleado y Cliente respectivamente
     Empleado emp1("Nylder", 20, 1500, "nylderff@gmail.com");
     Cliente cl1("Thunder", 23, 0, "thundergod@gmail.com");
     //mostramos la informacion de ambos objetos
